5443ab1b6bf442846648955bdedf0e9c8bcc2739
PraxTube <lukinator28@gmail.com>
2023-12-01 19:04:48 +0100

    feat!: Polish up repo

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Image Blending
 
+## Blended Images
+
+TODO
+
+## Laplace Operator
+
 The `laplace_operator` method will give us the following
 array for the input `5, 7`.
 
@@ -14,3 +20,20 @@ array for the input `5, 7`.
 ```
 
 The shape of this array is `(5 * 7, 5 * 7) = (35, 35)`.
+
+## What Went Wrong
+
+As you can see, the blending is clearly not working
+(neither the laplace blend, nor the mix blend).
+I suspect there to be some issues with the indices,
+but I could not for the life of me figure out the exact issue.
+One thing that really tripped me is the fact that mathematicians
+use column-major ordering to represent matrices while
+software engineers use row-major ordering.
+As I am not a mathematician, this led to many hours of confusion.
+
+It's also somewhat visible in the laplace blend in the plane-bird
+image. The pixels seems shifted in row/column which suggests that there
+is some bug with that indices.
+
+They grayscale of the images didn't reveal any insight regarding this bug either.
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+Script that blends a source image into a bigger target image.
+It only works for rectangular images, well it _should_ but
+there are some bugs so it doesn't really work.
+"""
+
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
@@ -5,6 +11,9 @@ from skimage import io
 
 
 def laplace_operator(m, n):
+    """
+    Compute laplace operator given m rows and n columns.
+    """
     I_m = scipy.sparse.identity(m)
     D_n = scipy.sparse.diags((-2, 1, 1), (0, 1, -1), (n, n))
 
@@ -14,16 +23,38 @@ def laplace_operator(m, n):
     return scipy.sparse.kron(I_m, D_n) + scipy.sparse.kron(I_n, D_m)
 
 
+def discrete_gradient(f):
+    """
+    Compute the discrete gradient of the given n x m matrix
+    """
+    n, m = f.shape
+    D_n = scipy.sparse.diags((1, -1), (0, 1), (n, n))
+    D_m = scipy.sparse.diags((1, -1), (0, 1), (m, m))
+
+    diff_x = D_n @ f
+    diff_y = f @ D_m.T
+
+    gradient = np.stack((diff_x, diff_y), axis=2)
+    return gradient
+
+
 def calculate_C(m, n):
+    """
+    Compute the outer 1's of the Laplace Matrix.
+    These 1's represent the columns of the edges of Omega.
+    """
     C = scipy.sparse.eye(m * n, dtype=int, format="lil")
     mask = np.zeros((m * n), dtype=bool)
     mask[m : m * n - m] = True
     C[mask, mask] = 0
-
     return C
 
 
 def calculate_F(m, n):
+    """
+    Compute the inner 1's of the Laplace Matrix.
+    These 1's represent the rows of the edges of Omega.
+    """
     E = scipy.sparse.eye(m * n, dtype=int, format="lil")
     F = scipy.sparse.eye(m * n, dtype=int, format="lil")
     F -= E
@@ -31,15 +62,18 @@ def calculate_F(m, n):
     mask = np.zeros((m * n,), dtype=bool)
     mask[m::m] = True
     mask[2 * m - 1 :: m] = True
+
     F[mask, mask] = 1
     mask = np.zeros((m * n), dtype=bool)
     mask[m * n - m :] = True
     F[mask, mask] = 0
-
     return F
 
 
 def calculate_H(m, n):
+    """
+    Compute the 1's which will mask the actual edge values of h.
+    """
     C = calculate_C(m, n)
     F = calculate_F(m, n)
     H = scipy.sparse.eye(m * n, dtype=int, format="lil")
@@ -50,6 +84,9 @@ def calculate_H(m, n):
 
 
 def calculate_A(m, n):
+    """
+    Compute the Laplace Matrix that takes into account the edges of Omega.
+    """
     C = calculate_C(m, n)
     F = calculate_F(m, n)
     H = calculate_H(m, n)
@@ -62,6 +99,10 @@ def calculate_A(m, n):
 
 
 def calculate_b(m, n, h, g):
+    """
+    Compute b, where b takes into account the edges of Omega.
+    B is the right hand side of our CG equation.
+    """
     CF = calculate_C(m, n) + calculate_F(m, n)
     H = calculate_H(m, n)
 
@@ -70,7 +111,36 @@ def calculate_b(m, n, h, g):
     return A @ b
 
 
+def calculate_div_v(n, m, h, g):
+    """
+    Calculate the divergenz of the vector field.
+    It takes into account the magnitude of both gradients.
+    """
+    div_h = discrete_gradient(h)
+    div_g = discrete_gradient(g)
+
+    mag_div_h = np.linalg.norm(div_h, axis=2)
+    mag_div_g = np.linalg.norm(div_g, axis=2)
+
+    mask = mag_div_h > mag_div_g
+    inv_mask = mag_div_h <= mag_div_g
+
+    mask = np.repeat(mask[:, :, np.newaxis], 2, axis=2)
+    inv_mask = np.repeat(inv_mask[:, :, np.newaxis], 2, axis=2)
+
+    v = div_h * mask + div_g * inv_mask
+
+    D_n = scipy.sparse.diags((1, -1), (0, -1), (n, n))
+    D_m = scipy.sparse.diags((1, -1), (0, -1), (m, m))
+
+    r = D_n @ v[:, :, 0] + v[:, :, 1] @ D_m.T
+    return r
+
+
 def naive_blending(h_star, g, p):
+    """
+    Copy paste the source into the target.
+    """
     m, n = g.shape
     p = (p[1], p[0])
     h_star[p[0] : p[0] + m, p[1] : p[1] + n] = g
@@ -78,13 +148,40 @@ def naive_blending(h_star, g, p):
 
 
 def laplace_blending(h_star, g, p):
+    """
+    Use laplace blending to blend the source into the target.
+    """
+    # The inner of Omega only. The edges will be set later.
     g = g[1:-1, 1:-1]
     m, n = g.shape
+    # Swap values as we need row first, column second (y, x)
     p = (p[1], p[0])
     h = h_star[p[0] : p[0] + m, p[1] : p[1] + n]
 
     D = laplace_operator(m, n)
     b = calculate_b(m, n, h, g)
+    x, exit_code = scipy.sparse.linalg.cg(D, b, maxiter=10000)
+
+    if exit_code != 0:
+        raise ValueError("CG was not successful, exit code is NOT zero")
+
+    if not (p[0] + m <= h_star.shape[0] and p[1] + n <= h_star.shape[1]):
+        raise ValueError("Given position is outside of target image")
+
+    h_star[p[0] : p[0] + m, p[1] : p[1] + n] = x.reshape((m, n))
+    return h_star
+
+
+def mix_blend(h_star, g, p):
+    # The inner of Omega only. The edges will be set later.
+    g = g[1:-1, 1:-1]
+    m, n = g.shape
+    # Swap values as we need row first, column second (y, x)
+    p = (p[1], p[0])
+    h = h_star[p[0] : p[0] + m, p[1] : p[1] + n]
+
+    D = laplace_operator(m, n)
+    b = calculate_div_v(m, n, h, g).flatten()
     x, exit_code = scipy.sparse.linalg.cg(D, b, maxiter=10000)
 
     if exit_code != 0:
@@ -105,65 +202,91 @@ def gray_laplace_blending(h, g, p):
     return image
 
 
-def bear_water_example(naive_blend=False):
+def bear_water_example(blend_type):
+    # Read in the images and set pixel offset `p`
     h = io.imread("input/water.jpg")
     g = io.imread("input/bear.jpg")
     p = (10, 80)
 
-    if naive_blend:
+    if blend_type == 0:
         red_image = naive_blending(h[:, :, 0].copy(), g[:, :, 0], p)
         green_image = naive_blending(h[:, :, 1].copy(), g[:, :, 1], p)
         blue_image = naive_blending(h[:, :, 2].copy(), g[:, :, 2], p)
-    else:
+    elif blend_type == 1:
         red_image = laplace_blending(h[:, :, 0].copy(), g[:, :, 0], p)
         green_image = laplace_blending(h[:, :, 1].copy(), g[:, :, 1], p)
         blue_image = laplace_blending(h[:, :, 2].copy(), g[:, :, 2], p)
+    elif blend_type == 2:
+        red_image = mix_blend(h[:, :, 0].copy(), g[:, :, 0], p)
+        green_image = mix_blend(h[:, :, 1].copy(), g[:, :, 1], p)
+        blue_image = mix_blend(h[:, :, 2].copy(), g[:, :, 2], p)
+    else:
+        raise ValueError("The given blend type does not exist. Must be between 0 - 2.")
 
     image = np.stack((red_image, green_image, blue_image), axis=2)
     return image
 
 
-def plane_bird_example(naive_blend=False):
+def plane_bird_example(blend_type):
+    # Read in the images and set pixel offset `p`
     h = io.imread("input/bird.jpg")
     g = io.imread("input/plane.jpg")
     p = (400, 50)
 
-    if naive_blend:
+    if blend_type == 0:
         red_image = naive_blending(h[:, :, 0].copy(), g[:, :, 0], p)
         green_image = naive_blending(h[:, :, 1].copy(), g[:, :, 1], p)
         blue_image = naive_blending(h[:, :, 2].copy(), g[:, :, 2], p)
-    else:
+    elif blend_type == 1:
         red_image = laplace_blending(h[:, :, 0].copy(), g[:, :, 0], p)
         green_image = laplace_blending(h[:, :, 1].copy(), g[:, :, 1], p)
         blue_image = laplace_blending(h[:, :, 2].copy(), g[:, :, 2], p)
+    elif blend_type == 2:
+        red_image = mix_blend(h[:, :, 0].copy(), g[:, :, 0], p)
+        green_image = mix_blend(h[:, :, 1].copy(), g[:, :, 1], p)
+        blue_image = mix_blend(h[:, :, 2].copy(), g[:, :, 2], p)
+    else:
+        raise ValueError("The given blend type does not exist. Must be between 0 - 2.")
 
     image = np.stack((red_image, green_image, blue_image), axis=2)
     return image
 
 
 def main():
-    print("WARNING: This will take some time. The program will compute a lot of redundant matrices.")
-    images = [
-        bear_water_example(True),
-        plane_bird_example(True),
-        bear_water_example(),
-        plane_bird_example(),
-    ]
+    print(
+        "WARNING: This will take some time. The program will compute a lot of redundant matrices."
+    )
+    # Define values for displaying the blended images
+    col = 3
+    row = 2
+    images = []
     titles = [
-        "Naive Blend - Bear in Water",
-        "Naive Blend - Plane and Bird",
-        "Laplace Blend - Bear in Water",
-        "Laplace Blend - Plane and Bird",
+        [
+            "Naive Blend - Bear in Water",
+            "Laplace Blend - Bear in Water",
+            "Mix Blend - Bear in Water",
+        ],
+        [
+            "Naive Blend - Plane and Bird",
+            "Laplace Blend - Plane and Bird",
+            "Mix Blend - Plane and Bird",
+        ],
     ]
+    for i in range(3):
+        images.append(bear_water_example(i))
+        print(f"Generated: {titles[0][i]}")
+        images.append(plane_bird_example(i))
+        print(f"Generated: {titles[1][i]}")
 
     # Create a figure and subplots based on the number of images
-    fig, axes = plt.subplots(2, 2, figsize=(2 * 5, 2 * 5))
+    fig, axes = plt.subplots(row, col, figsize=(col * 5, row * 5))
 
+    # Blend all images and append to `images`
     for i in range(len(images)):
-        row = i // 2
-        col = i % 2
+        row = i % 2
+        col = i // 2
         axes[row, col].imshow(images[i])
-        axes[row, col].set_title(titles[i])
+        axes[row, col].set_title(titles[row][col])
         axes[row, col].axis("off")
 
     plt.tight_layout()
@@ -171,5 +294,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # main()
-    print(laplace_operator(5, 7).toarray())
+    main()
